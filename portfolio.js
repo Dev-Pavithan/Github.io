5b3c39d81eb37bc43248914fcb98f4684e7d146c
Dev-Pavithan <pavithanunenthiran29@gmail.com>
2024-05-28 12:29:29 +0530

    first change

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+   strings: ["Student Developer", "UI/UX Designer", "Content Creater"],
+   typespeed: 10,
+   backspeed: 10,
+   backdelay: 10,
+   loop: true,
+ });
